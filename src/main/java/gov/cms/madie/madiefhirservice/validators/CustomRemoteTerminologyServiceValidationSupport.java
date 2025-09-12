278ca3449f88c8dba71c2da1b8ec37735ec94fb1
adongare <ashok.dongare@semanticbits.com>
2025-09-12 13:49:20 -0400

    MAT-9168 Fix Issues with Errors displaying When Binding strength is Preferred or Extensible

--- src/main/java/gov/cms/madie/madiefhirservice/validators/CustomRemoteTerminologyServiceValidationSupport.java
+++ src/main/java/gov/cms/madie/madiefhirservice/validators/CustomRemoteTerminologyServiceValidationSupport.java
@@ -2,6 +2,7 @@ package gov.cms.madie.madiefhirservice.validators;
 
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.context.support.ConceptValidationOptions;
+import ca.uhn.fhir.context.support.DefaultProfileValidationSupport;
 import ca.uhn.fhir.context.support.ValidationSupportContext;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import ca.uhn.fhir.rest.client.interceptor.BasicAuthInterceptor;
@@ -13,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.hl7.fhir.common.hapi.validation.support.RemoteTerminologyServiceValidationSupport;
+import org.hl7.fhir.common.hapi.validation.support.ValidationSupportUtils;
 import org.hl7.fhir.instance.model.api.IBaseBundle;
 import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.hl7.fhir.r4.model.CodeSystem;
@@ -21,11 +23,14 @@ import org.hl7.fhir.r4.model.ValueSet;
 
 import java.util.List;
 
+import static org.apache.commons.lang3.StringUtils.isBlank;
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+
 @Slf4j
 public class CustomRemoteTerminologyServiceValidationSupport
     extends RemoteTerminologyServiceValidationSupport {
   private final IGenericClient client;
-  private ValidationConfig validationConfig;
+  private final ValidationConfig validationConfig;
 
   public CustomRemoteTerminologyServiceValidationSupport(
       FhirContext theFhirContext,
@@ -69,34 +74,17 @@ public class CustomRemoteTerminologyServiceValidationSupport
       String theDisplay,
       String theValueSetUrl) {
     log.info(
-        "called with: theCodeSystem={}, theCode={}, theDisplay={}, theValueSetUrl={}",
+        "validateCode called with: theCodeSystem={}, theCode={}, theDisplay={}, theValueSetUrl={}",
         theCodeSystem,
         theCode,
         theDisplay,
         theValueSetUrl);
-    return superValidateCode(
-        theValidationSupportContext,
-        theOptions,
+    return invokeValidateCode(
         theCodeSystem,
         theCode,
         validationConfig.isValidateDisplay() ? theDisplay : null,
-        theValueSetUrl);
-  }
-
-  protected CodeValidationResult superValidateCode(
-      ValidationSupportContext theValidationSupportContext,
-      ConceptValidationOptions theOptions,
-      String theCodeSystem,
-      String theCode,
-      String theDisplay,
-      String theValueSetUrl) {
-    return super.validateCode(
-        theValidationSupportContext,
-        theOptions,
-        theCodeSystem,
-        theCode,
-        theDisplay,
-        theValueSetUrl);
+        theValueSetUrl,
+        null);
   }
 
   @Override
@@ -108,24 +96,23 @@ public class CustomRemoteTerminologyServiceValidationSupport
       String theDisplay,
       @Nonnull IBaseResource theValueSet) {
 
-    return superValidateCodeInValueSet(
-        theValidationSupportContext,
-        theOptions,
-        theCodeSystem,
-        theCode,
-        validationConfig.isValidateDisplay() ? theDisplay : null,
-        theValueSet);
-  }
+    IBaseResource valueSet = theValueSet;
 
-  public CodeValidationResult superValidateCodeInValueSet(
-      ValidationSupportContext theValidationSupportContext,
-      ConceptValidationOptions theOptions,
-      String theCodeSystem,
-      String theCode,
-      String theDisplay,
-      @Valid IBaseResource theValueSet) {
-    return super.validateCodeInValueSet(
-        theValidationSupportContext, theOptions, theCodeSystem, theCode, theDisplay, theValueSet);
+    // some external validators require the system when the code is passed
+    // so let's try to get it from the VS if is not present
+    String codeSystem = theCodeSystem;
+    if (isNotBlank(theCode) && isBlank(codeSystem)) {
+      codeSystem = ValidationSupportUtils.extractCodeSystemForCode(theValueSet, theCode);
+    }
+
+    String valueSetUrl = DefaultProfileValidationSupport.getConformanceResourceUrl(myCtx, valueSet);
+    if (isNotBlank(valueSetUrl)) {
+      valueSet = null;
+    } else {
+      valueSetUrl = null;
+    }
+    String display = validationConfig.isValidateDisplay() ? theDisplay : null;
+    return invokeValidateCode(codeSystem, theCode, display, valueSetUrl, valueSet);
   }
 
   @Override
@@ -139,7 +126,7 @@ public class CustomRemoteTerminologyServiceValidationSupport
   public IBaseResource fetchValueSet(String theValueSetUrl) {
     // disable code value set fetch for remote terminology service because it appears that VSAC
     // doesn't support value set search with summary mode
-    // Added cases it calls for cts valuesets
+    // Added cases it calls for cts value sets
     if (StringUtils.isBlank(theValueSetUrl)
         || !StringUtils.contains(theValueSetUrl, "cts.nlm.nih.gov")) {
       return null;
@@ -155,5 +142,46 @@ public class CustomRemoteTerminologyServiceValidationSupport
       List<IBaseResource> resultsList = BundleUtil.toListOfResources(this.myCtx, results);
       return !resultsList.isEmpty() ? resultsList.get(0) : null;
     }
+  }
+
+  protected CodeValidationResult invokeSuperRemoteValidateCode(
+      String theCodeSystem,
+      String theCode,
+      String theDisplay,
+      String theValueSetUrl,
+      @Valid IBaseResource theValueSet) {
+    return super.invokeRemoteValidateCode(
+        theCodeSystem, theCode, theDisplay, theValueSetUrl, theValueSet);
+  }
+
+  private CodeValidationResult invokeValidateCode(
+      String theCodeSystem,
+      String theCode,
+      String theDisplay,
+      String theValueSetUrl,
+      @Valid IBaseResource theValueSet) {
+    CodeValidationResult validationResult =
+        invokeSuperRemoteValidateCode(
+            theCodeSystem, theCode, theDisplay, theValueSetUrl, theValueSet);
+    List<CodeValidationIssue> validationIssues = validationResult.getIssues();
+    // this is to get away with HAPI's remote terminology service validation support not able to
+    // validate non-required bindings correctly.
+    // RemoteTerminologyServiceValidationSupport.invokeRemoteValidateCode
+    // returns "invalid-code" when the code is not found in the value set or code system(that's
+    // hardcoded).
+    // However, binding strength check expects it to be "not-found".
+    // This ugly fixup is to change the issue code from "invalid-code" to "not-found"
+    // TODO: revisit this when HAPI fixes it
+    if (validationResult.getSeverity() == IssueSeverity.ERROR) {
+      if (CollectionUtils.isNotEmpty(validationIssues)) {
+        CodeValidationIssueDetails issueDetails = validationIssues.get(0).getDetails();
+        CodeValidationIssueCoding issueCoding = issueDetails.getCodings().get(0);
+        if (StringUtils.equals(issueCoding.getCode(), "invalid-code")) {
+          issueDetails.addCoding(CodeValidationIssueCoding.TX_ISSUE_SYSTEM, "not-found");
+          issueDetails.getCodings().remove(issueCoding);
+        }
+      }
+    }
+    return validationResult;
   }
 }
