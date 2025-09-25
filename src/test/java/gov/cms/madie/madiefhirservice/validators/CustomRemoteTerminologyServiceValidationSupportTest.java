09979788064164700e07c83722bf181e3fcebb1f
adongare <ashok.dongare@semanticbits.com>
2025-09-25 11:41:08 -0400

    MAT-9115 revert the HAPI FHIR update that is breaking in-memory terminology validation

--- src/test/java/gov/cms/madie/madiefhirservice/validators/CustomRemoteTerminologyServiceValidationSupportTest.java
+++ src/test/java/gov/cms/madie/madiefhirservice/validators/CustomRemoteTerminologyServiceValidationSupportTest.java
@@ -586,14 +586,11 @@ class CustomRemoteTerminologyServiceValidationSupportTest {
     when(query.execute()).thenReturn(bundle);
 
     List<IBaseResource> mockResources = List.of(codeSystemResource);
-    // Ensure static mock is closed after use to avoid leaking into other tests
-    try (MockedStatic<BundleUtil> bundleUtilMock = mockStatic(BundleUtil.class)) {
-      bundleUtilMock
-          .when(() -> BundleUtil.toListOfResources(fhirContext, bundle))
-          .thenReturn(mockResources);
+    mockStatic(BundleUtil.class);
+    when(BundleUtil.toListOfResources(fhirContext, bundle)).thenReturn(mockResources);
 
-      IBaseResource result = validationSupport.fetchValueSet(validCtsUrl);
-      assertThat(result, is(codeSystemResource));
-    }
+    IBaseResource result = validationSupport.fetchValueSet(validCtsUrl);
+
+    assertThat(result, is(codeSystemResource));
   }
 }
