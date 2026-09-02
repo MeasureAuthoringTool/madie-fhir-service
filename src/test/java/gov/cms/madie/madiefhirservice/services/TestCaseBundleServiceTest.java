6196cc44bbbf09c201d70a26cfb4a18cb0ab39fd
adongare <ashok.dongare@semanticbits.com>
2026-09-02 17:00:23 -0400

    MAT-10436 Update Profile URLs - QMIG 2 Updates

--- src/test/java/gov/cms/madie/madiefhirservice/services/TestCaseBundleServiceTest.java
+++ src/test/java/gov/cms/madie/madiefhirservice/services/TestCaseBundleServiceTest.java
@@ -57,7 +57,6 @@ import gov.cms.madie.packaging.utils.qicore411.PackagingUtilityImpl;
 
 @ExtendWith(MockitoExtension.class)
 class TestCaseBundleServiceTest implements ResourceFileUtil {
-
   @InjectMocks private TestCaseBundleService testCaseBundleService;
 
   @Spy private FhirContext fhirContext;
